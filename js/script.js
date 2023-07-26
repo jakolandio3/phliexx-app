8cfba8143ec72b1ca9193b8218ffced5d5c6793c
Jakob Douglas <jakobdouglas1@gmail.com>
2023-07-26 17:53:11 +0930

    slider finished

--- js/script.js
+++ js/script.js
@@ -224,6 +224,56 @@ async function displayShowDetails() {
 	console.log(showID);
 }
 
+// display slider movies
+async function displaySlider() {
+	const { results } = await fetchAPIData('movie/now_playing');
+	results.forEach((movie) => {
+		const newDiv = document.createElement('div');
+		newDiv.classList.add('swiper-slide');
+		newDiv.innerHTML = `
+  					
+            <a href="movie-details.html?id=${movie.id}">
+              ${
+								movie.poster_path
+									? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />`
+									: `<img src="./images/no-image.jpg" alt="${movie.title}" />`
+							}
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${
+								movie.vote_average
+							} / 10
+            </h4>
+          `;
+		document.querySelector('.swiper-wrapper').appendChild(newDiv);
+		initSwiper();
+	});
+}
+// init swiper
+function initSwiper() {
+	const swiper = new Swiper('.swiper', {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		freeMode: true,
+		loop: true,
+		autoplay: {
+			delay: 4000,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			500: {
+				slidesPerView: 2,
+			},
+			700: {
+				slidesPerView: 3,
+			},
+			1200: {
+				slidesPerView: 4,
+			},
+		},
+	});
+}
+
 // create backdrop function
 function displayBackgroundImage(type, backgroundpath) {
 	const overlayDiv = document.createElement('div');
@@ -280,6 +330,7 @@ function init() {
 		case '/':
 		case '/index.html':
 			displayPopularMovies();
+			displaySlider();
 			console.log('Home');
 			break;
 		case '/shows.html':
